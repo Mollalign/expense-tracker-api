556ca41ddf39a8656d59c351ad8d89b4e473f22b
Mollalign <molledan69@gmail.com>
2025-08-11 18:04:50 +0300

    Add CORS support and update middleware configuration

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'expenses',
     'ninja',
     'rest_framework',
-    'users'
+    'users',
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # your Next.js frontend URL
 ]
 
 ROOT_URLCONF = 'expense_tracker.urls'
